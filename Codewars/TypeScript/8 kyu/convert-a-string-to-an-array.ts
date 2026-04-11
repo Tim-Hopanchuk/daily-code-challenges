25f5f5b72c56de2e358d39ed4b4340676bdf4e13
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-11 20:19:03 +0300

    feat(codewars): solve 8 kyu kata "Convert a string to an array"

---
+++ Codewars/TypeScript/8 kyu/convert-a-string-to-an-array.ts
@@ -0,0 +1,10 @@
+/*
+  Convert a string to an array
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+*/
+
+export function stringToArray(str: string): string[] {
+  return str.split(" ");
+}
