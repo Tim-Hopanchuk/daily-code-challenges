f8f7204945ac57b122d57c47806fc7cefdaf1bba
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-12 18:45:17 +0300

    feat(codewars): solve 8 kyu kata "Sum The Strings"

---
+++ Codewars/TypeScript/8 kyu/sum-the-strings.ts
@@ -0,0 +1,10 @@
+/*
+  Sum The Strings
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/5966e33c4e686b508700002d
+*/
+
+export function sumStr(a: string, b: string): string {
+  return String(Number(a) + Number(b));
+}
