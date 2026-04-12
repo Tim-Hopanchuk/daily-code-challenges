603b10b42e69afefa9f6bf4307ff40167547e93a
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-12 18:42:35 +0300

    feat(codewars): solve 8 kyu kata "Count Odd Numbers below n"

---
+++ Codewars/TypeScript/8 kyu/count-odd-numbers-below-n.ts
@@ -0,0 +1,10 @@
+/*
+  Count Odd Numbers below n
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/59342039eb450e39970000a6
+*/
+
+export function oddCount(n: number) {
+  return Math.floor(n / 2);
+}
