6b684cdd1b837320e12d0dcae6c99f9f71ec5c9c
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-13 12:13:21 +0300

    feat(codewars): solve 8 kyu kata "Is the string uppercase?"

---
+++ Codewars/TypeScript/8 kyu/is-the-string-uppercase.ts
@@ -0,0 +1,10 @@
+/*
+  Is the string uppercase?
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+*/
+
+export function isUpperCase(str: string): boolean {
+  return str === str.toUpperCase();
+}
