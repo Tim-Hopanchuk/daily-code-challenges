ccdf492ce3d40d0a0c9794c0c27da7644e22dc3e
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-13 12:12:17 +0300

    feat(codewars): solve 8 kyu kata "Grasshopper - Debug sayHello"

---
+++ Codewars/TypeScript/8 kyu/grasshopper-debug-sayhello.ts
@@ -0,0 +1,10 @@
+/*
+  Grasshopper - Debug sayHello
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/5625618b1fe21ab49f00001f
+*/
+
+export function sayHello(name: string): string {
+  return "Hello, " + name;
+}
