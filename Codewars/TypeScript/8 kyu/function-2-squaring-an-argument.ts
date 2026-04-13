871157b20907e68237ebc1f1a89f3b1364ca49be
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-13 12:11:44 +0300

    feat(codewars): solve 8 kyu kata "Function 2 - squaring an argument"

---
+++ Codewars/TypeScript/8 kyu/function-2-squaring-an-argument.ts
@@ -0,0 +1,10 @@
+/*
+  Function 2 - squaring an argument
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/523b623152af8a30c6000027
+*/
+
+export function square(num: number): number {
+  return num ** 2;
+}
