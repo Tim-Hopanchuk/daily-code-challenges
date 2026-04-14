58698a3d8debe1f9455cfb549245983d25175bbe
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-14 15:09:31 +0300

    feat(codewars): solve 8 kyu kata "Basic variable assignment"

---
+++ Codewars/TypeScript/8 kyu/basic-variable-assignment.ts
@@ -0,0 +1,10 @@
+/*
+  Basic variable assignment
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/50ee6b0bdeab583673000025
+*/
+
+const a = "code";
+const b = "wa.rs";
+export const name = a + b;
