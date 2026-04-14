6751781a4c5c0d03e205faaef865a5602c0927a6
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-14 15:13:59 +0300

    feat(codewars): solve 8 kyu kata "Reverse List Order"

---
+++ Codewars/TypeScript/8 kyu/reverse-list-order.ts
@@ -0,0 +1,10 @@
+/*
+  Reverse List Order
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
+*/
+
+export function reverseList(arr: number[]): number[] {
+  return arr.reverse();
+}
