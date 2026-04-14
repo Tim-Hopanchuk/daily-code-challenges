0c609b871fac2169067c2d88bc9f3cfa2313cf69
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-14 15:12:37 +0300

    feat(codewars): solve 8 kyu kata "Grasshopper - If/else syntax debug"

---
+++ Codewars/TypeScript/8 kyu/grasshopper-If-else-syntax-debug.ts
@@ -0,0 +1,14 @@
+/*
+  Grasshopper - If/else syntax debug
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/57089707fe2d01529f00024a
+*/
+
+export function checkAlive(health: number): boolean {
+  if (health <= 0) {
+    return false;
+  }
+
+  return true;
+}
