d8b839313edca824533d83e51b464a4379817a43
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-14 15:13:28 +0300

    feat(codewars): solve 8 kyu kata "My head is at the wrong end!"

---
+++ Codewars/TypeScript/8 kyu/my-head-is-at-the-wrong-end.ts
@@ -0,0 +1,10 @@
+/*
+  My head is at the wrong end!
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/56f699cd9400f5b7d8000b55
+*/
+
+export function fixTheMeerkat(arr: string[]): string[] {
+  return arr.reverse();
+}
