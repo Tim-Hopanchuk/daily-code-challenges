b4ea40c66d5159e536110dbe9d3f964b96a9a1c3
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-14 15:09:55 +0300

    feat(codewars): solve 8 kyu kata "Exclusive "or" (xor) Logical Operator"

---
+++ Codewars/TypeScript/8 kyu/exclusive-or-xor-logical-operator.ts
@@ -0,0 +1,10 @@
+/*
+  Exclusive "or" (xor) Logical Operator
+  
+  Difficulty: 8 kyu
+  Link: https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c
+*/
+
+export function xor(a: boolean, b: boolean): boolean {
+  return a !== b;
+}
