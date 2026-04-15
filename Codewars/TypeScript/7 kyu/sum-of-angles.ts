acae8b51bd618aabcb79c82ec7a27f1f500f0c19
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-04-15 19:13:24 +0300

    feat(codewars): solve 7 kyu kata "Sum of angles"

---
+++ Codewars/TypeScript/7 kyu/sum-of-angles.ts
@@ -0,0 +1,10 @@
+/*
+  Sum of angles
+  
+  Difficulty: 7 kyu
+  Link: https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+*/
+
+export function angle(n: number): number {
+  return 180 * (n - 2);
+}
