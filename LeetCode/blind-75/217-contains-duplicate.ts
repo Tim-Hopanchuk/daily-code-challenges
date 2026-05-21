174592aa8d25d4e807900f87e35be7a070d6215f
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-05-21 18:26:11 +0300

    feat(leetcode): solve easy problem "217. Contains Duplicate"

---
+++ LeetCode/blind-75/217-contains-duplicate.ts
@@ -0,0 +1,31 @@
+/*
+  217. Contains Duplicate
+  
+  Difficulty: Easy
+  Topics: Array, Hash Table, Sorting
+  Runtime: 19 ms
+  Memory: 73.81 MB
+
+
+  Link: https://leetcode.com/problems/contains-duplicate?envType=problem-list-v2&envId=oizxjoit
+*/
+
+function containsDuplicate(nums: number[]): boolean {
+  return nums.length > new Set(nums).size;
+}
+
+/*
+  Alternatives:
+
+  function containsDuplicate(nums: number[]): boolean {
+    const sortedNums = nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < sortedNums.length - 1; i++) {
+      if (sortedNums[i] === sortedNums[i + 1]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+*/
