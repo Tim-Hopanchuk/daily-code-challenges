45c3e1621800e925574f42e935dbcd4a35f3bffe
Tim-Hopanchuk <tim.hpnk@gmail.com>
2026-07-03 21:27:12 +0300

    feat(leetcode): add alternative solution to easy problem "169. Majority Element"

--- LeetCode/neetcode-250/169-majority-element.ts
+++ LeetCode/neetcode-250/169-majority-element.ts
@@ -3,25 +3,31 @@
   
   Difficulty: Easy
   Topics: Array, Hash Table, Divide and Conquer, Sorting, Counting
-  Runtime: 4 ms
-  Memory: 59.4 MB
+  Runtime: 0 ms
+  Memory: 58.17 MB
 
   Link: https://leetcode.com/problems/majority-element
 */
 
-function majorityElement1(nums: number[]): number {
-  const signatures = new Map<number, number>();
+// Boyer–Moore majority vote algorithm
 
-  for (const num of nums) {
-    let count = (signatures.get(num) ?? 0) + 1;
-    if (count > nums.length / 2) {
-      return num;
+function majorityElement(nums: number[]): number {
+  let candidate = nums[0];
+  let count = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
     }
 
-    signatures.set(num, count);
+    if (candidate === nums[i]) {
+      count++;
+    } else {
+      count--;
+    }
   }
 
-  return nums[0];
+  return candidate;
 }
 
 /*
@@ -50,5 +56,20 @@ function majorityElement1(nums: number[]): number {
     const sortedNums = [...nums].sort((a, b) => a - b);
 
     return sortedNums[Math.floor(sortedNums.length / 2)];
+  }
+
+  function majorityElement(nums: number[]): number {
+    const signatures = new Map<number, number>();
+
+    for (const num of nums) {
+      let count = (signatures.get(num) ?? 0) + 1;
+      if (count > nums.length / 2) {
+        return num;
+      }
+
+      signatures.set(num, count);
+    }
+
+    return nums[0];
   }
 */
